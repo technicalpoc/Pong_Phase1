57fdd418ed5fd710be3127d62ff9bdabd67957a0
Rashmit Rathod <rashmit.rathod@yahoo.com>
2015-11-23 12:19:09 +0530

    code updated

--- src/main/java/org/bk/producer/controller/PongController.java
+++ src/main/java/org/bk/producer/controller/PongController.java
@@ -4,13 +4,17 @@ package org.bk.producer.controller;
 import org.bk.producer.domain.Message;
 import org.bk.producer.domain.MessageAcknowledgement;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableAutoConfiguration
+@ComponentScan
 @RestController
 @RefreshScope
 public class PongController {
